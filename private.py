e14dbc406a1bcbcc00c36adf9fe79a3671d09fb4
Steven Swanson <swanson@cs.ucsd.edu>
2019-12-24 18:40:42 +0000

    fix obvious error

--- private.py
+++ private.py
@@ -6,19 +6,28 @@ from lab import ThisLab as PublicLab
 
 class ThisLab(PublicLab):
 
+    # These are tests for the student's code.  Gradescope will run
+    # them and score the lab accordingly.
     class GradedRegressions(PublicLab.GradedRegressions):
+        def setUp(self):
+            CSE141Lab.GradedRegressions.setUp(self)
+            self.lab_spec = ThisLab.load(".")
 
-        # the numbers in the name are improtant.  They set the order
-        # things run in.
+
+        @weight(3)
+        def test_getting_this_far(self):
+            self.assertTrue(True)
+
+        @weight(3)
+        def test_for_correct_output(self):
+            with open("code.out") as f:
+                contents = f.read()
+            self.assertNotRegex(contents, 'Your Name', "I found 'Your Name' in your 'code.out'.  It shouldn't be there.")
+            self.assertRegex(contents, 'hi', "I couldn't find text match 'hi,' in your output")
+
         @weight(1)
-        def test_0_regressions(self):
-            self.go_run_tests("")
+        def test_starter_output(self):
+            c = self.read_file("outputs/starter-output.txt")
+            self.assertRegex(c, "Hello, world!", "I couldn't find 'Hello, world!' in your 'outputs/starter-output.txt'")
 
-        @weight(1)
-        def test_0_answer(self):
-            self.assertEqual(self.read_file('answer.out',root=".").strip(), "correct answer")
 
-        @leaderboard("winning")
-        def test_98_leaderboard(self, set_leaderboard_value=None):
-            set_leaderboard_value(self.csv_extract_by_line(result.files['code-stats.csv'], 'magic'))
-            
